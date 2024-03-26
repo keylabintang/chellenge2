6a2898e8f66c02d9dd76b385eadb9442ed202217
abiin21 <keyla.putrie.ti.21@cic.id>
2024-03-27 00:20:30 +0700

    first commit

---
+++ no1.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukan skor Lumba-lumba juri a : ");
+  var lumbalumba_juri_a = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukan skor Lumba-lumba juri b : ");
+  var lumbalumba_juri_b = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukan skor Lumba-lumba juri c : ");
+  var lumbalumba_juri_c = int.parse(stdin.readLineSync()!);
+
+  // Data skor Lumba-lumba
+  List<int> lumbaLumbaSkor = [
+    lumbalumba_juri_a,
+    lumbalumba_juri_b,
+    lumbalumba_juri_c
+  ];
+
+  stdout.write("Masukan skor Koala juri a : ");
+  var koala_juri_a = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukan skor Koala juri b : ");
+  var koala_juri_b = int.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukan skor Koala juri c : ");
+  var koala_juri_c = int.parse(stdin.readLineSync()!);
+
+  // Data skor Lumba-lumba
+  List<int> koalaSkor = [koala_juri_a, koala_juri_b, koala_juri_c];
+
+  // Menghitung skor rata-rata untuk setiap tim
+  double rataRataLumbaLumba = menghitung_ratarata(lumbaLumbaSkor);
+  double rataRataKoala = menghitung_ratarata(koalaSkor);
+
+  // Menampilkan hasil
+  print(
+      "Skor rata-rata Tim Lumba-lumba: ${rataRataLumbaLumba.toStringAsFixed(2)}");
+  print("Skor rata-rata Tim Koala: ${rataRataKoala.toStringAsFixed(2)}");
+  print(menentukan_pemenang(rataRataLumbaLumba, rataRataKoala));
+}
+
+double menghitung_ratarata(List<int> skors) {
+  double total = 0;
+  for (int skor in skors) {
+    total += skor;
+  }
+  return total / skors.length;
+}
+
+String menentukan_pemenang(double lumbaLumba, double koala) {
+  if (lumbaLumba > koala && lumbaLumba >= 100) {
+    return "Selamat! Tim Lumba-lumba menang!";
+  } else if (koala > lumbaLumba && koala >= 100) {
+    return "Selamat! Tim Koala menang!";
+  } else if (lumbaLumba == koala && lumbaLumba >= 100) {
+    return "Hasil Seri! Kedua tim memiliki memiliki skor yang sama";
+  } else {
+    return "Tidak ada pemenang! Tidak ada tim yang mencapai skor 100.";
+  }
+}
---
+++ no2.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:io';
+
+void main() {
+  //Mark
+  stdout.write("Masukkan Massa badan Mark: ");
+  var massa_mark = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan Tinggi badan Mark: ");
+  var tinggi_mark = double.parse(stdin.readLineSync()!);
+
+  //Perhitungan BMI Mark
+  var BMI_mark = (massa_mark / (tinggi_mark * tinggi_mark));
+
+  print("Mark memiliki BMI : ${BMI_mark.toStringAsFixed(2)}");
+
+  //John
+  stdout.write("Masukkan Massa badan John: ");
+  var massa_john = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Masukkan Tinggi badan John: ");
+  var tinggi_john = double.parse(stdin.readLineSync()!);
+
+  //Perhitungan BMI John
+  var BMI_john = (massa_john / (tinggi_john * tinggi_john));
+
+  print("Mark memiliki BMI : ${BMI_john.toStringAsFixed(2)}");
+
+  // Menentukan apakah Mark memiliki BMI lebih tinggi dari John
+  var markHigherBMI = BMI_mark > BMI_john;
+  print(
+      "Apakah Mark memiliki nilai BMI lebih tinggi dari pada John? $markHigherBMI");
+}
